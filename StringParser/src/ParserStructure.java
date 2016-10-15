83d84c5229394ffc308314b76cbd21b6b2dab307
as13t <arronsolano@yahoo.com>
2016-10-15 08:45:36 -0400

    Update to ParserStruct

--- StringParser/src/ParserStructure.java
+++ StringParser/src/ParserStructure.java
@@ -1,5 +1,10 @@
 import java.util.HashMap;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 // This will be using the hashmap to keep a running count of all words parsed within the given text.
 
 public class ParserStructure 
@@ -26,10 +31,30 @@ public class ParserStructure
 	
 	public void displayMap()
 	{
-		for(String key: hm.keySet())
+		/*for(String key: hm.keySet())
 		{
 			System.out.println(key + " = " + hm.get(key));
+		}*/
+		JFrame dFrame = new JFrame();
+		JPanel pt = new JPanel();
+		DefaultTableModel dtm = new DefaultTableModel();
+		
+		dtm.addColumn("Word");
+		dtm.addColumn("Count");
+		
+		JTable tb = new JTable(dtm);
+		
+		for(String key: hm.keySet())
+		{
+			dtm.addRow(new Object[] {key, hm.get(key)});
 		}
+		
+		pt.add(tb);
+		dFrame.add(pt);
+		
+		dFrame.setSize(600, 600);
+		dFrame.setVisible(true);
+		dFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}
 	
 }
